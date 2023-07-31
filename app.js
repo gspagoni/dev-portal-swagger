df6701942eb70ae7ccd1e9e114f39998502f7756
Giampaolo Spagoni <gspagoni@outlook.com>
2023-07-31 18:58:41 +0200

    added all files

---
+++ app.js
@@ -0,0 +1,40 @@
+const os = require("os")
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const path = require("path");
+require('dotenv').config()
+
+const hostName = os.hostname()
+console.log(hostName)
+const PORT = process.env.PORT || 3100
+
+const app = express()
+app.use(express.static("swagger-ui"));
+
+var options = {
+    explorer: true,
+    swaggerOptions: {
+      urls: [
+        {
+          url: `http://${hostName}:${PORT}/idm.json`,
+          name: 'IDM'
+        },
+        {
+          url: `http://${hostName}:${PORT}/mingle.json`,
+          name: 'Mingle'
+        }
+      ]
+    }
+  }
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options));
+
+  app.post('/api-docs/oauth2-redirect.html',(req,res)=>{
+    // this is a fake url used by swagger authorization
+  })
+
+
+
+
+app.listen(PORT,()=> console.log(`server started at http://${hostName}:${PORT}`))
